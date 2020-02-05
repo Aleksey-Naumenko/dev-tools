bd1f6a411df20689bf96146e8563953cc48a3f0a
Aleksey <aleksey_naumenko@ukr.net>
2020-02-05 15:17:29 +0200

    eslint calendar

--- lesson9/hw1/task1/src/index.js
+++
@@ -1,4 +0,0 @@
-import { setItem, getItem, onStorageChange } from './list/storage.js';
-import { renderTasksList } from './list/render-tasks.js';
-import { onTasksListChange } from './list/on-taskslist-change.js';
-import { addNewTask } from './list/create-new-task.js';
